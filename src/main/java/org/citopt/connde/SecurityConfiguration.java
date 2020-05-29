ff5f96450503128052f28286e7acbb708167d3b3
Jan Schneider <st117301@stud.uni-stuttgart.de>
2020-05-29 23:08:57 +0200

    Added CORS support by modifying the web security configuration

--- src/main/java/org/citopt/connde/SecurityConfiguration.java
+++ src/main/java/org/citopt/connde/SecurityConfiguration.java
@@ -17,9 +17,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Security configuration
+ *
  * @author Imeri Amil
  */
 
@@ -28,61 +35,75 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public RestAuthenticationEntryPoint restAuthenticationEntryPoint() {
-		return new RestAuthenticationEntryPoint();
-	}
+    @Bean
+    public RestAuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return new RestAuthenticationEntryPoint();
+    }
 
     @Bean
     public UserDetailsService mongoUserDetails() {
         return new UserDetailsServiceImpl();
     }
 
-	@Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-	@Override
+    @Override
     public void configure(AuthenticationManagerBuilder auth) {
         try {
-        	UserDetailsService userDetailsService = mongoUserDetails();
+            UserDetailsService userDetailsService = mongoUserDetails();
             auth
-                .userDetailsService(userDetailsService)
+                    .userDetailsService(userDetailsService)
                     .passwordEncoder(passwordEncoder());
         } catch (Exception e) {
             throw new BeanInitializationException("Security configuration failed", e);
         }
     }
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-	    web.ignoring()
-	    .antMatchers(HttpMethod.OPTIONS, "/**")
-	    .antMatchers("/resources/**")
-	    .antMatchers("/webapp/**");
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/resources/**")
+                .antMatchers("/webapp/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        	.csrf().disable()
-        	.authorizeRequests()
-        	.antMatchers("/api/authenticate").permitAll()
-        	.antMatchers(HttpMethod.POST, "/api/users").permitAll()
-        	.antMatchers(HttpMethod.PUT, "/api/users").hasAuthority(Constants.ADMIN)
-        	.antMatchers(HttpMethod.GET, "/api/users").hasAuthority(Constants.ADMIN)
-        	.antMatchers(HttpMethod.GET, "/api/users/:username").hasAuthority(Constants.ADMIN)
-        	.antMatchers(HttpMethod.DELETE, "/api/users/:username").hasAuthority(Constants.ADMIN)
-        	.antMatchers("/api/**").authenticated()
-		.and()
-			.httpBasic()
-        	.authenticationEntryPoint(restAuthenticationEntryPoint())
-        .and()
-        	.logout()
-        	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-        	.logoutSuccessUrl("/login")
-        	.invalidateHttpSession(true)
-        	.deleteCookies("JSESSIONID");
+                .cors().and() //Configure CORS
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/users").permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/users").hasAuthority(Constants.ADMIN)
+                .antMatchers(HttpMethod.GET, "/api/users").hasAuthority(Constants.ADMIN)
+                .antMatchers(HttpMethod.GET, "/api/users/:username").hasAuthority(Constants.ADMIN)
+                .antMatchers(HttpMethod.DELETE, "/api/users/:username").hasAuthority(Constants.ADMIN)
+                .antMatchers("/api/**").authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(restAuthenticationEntryPoint())
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+    }
+
+    /**
+     * Creates a bean configuring the CORS support.
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*", "/**"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
